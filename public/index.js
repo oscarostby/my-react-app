d2d52e638b3b46e0f5de3e5abe46302750ee0e5d
rompestomp <playzlama1@gmail.com>
2023-10-25 11:34:36 +0200

    Co-authored-by: Maamua06 <Maamua06@users.noreply.github.com> Co-authored-by: mohamad7s <mohamad7s@users.noreply.github.com> Co-authored-by: Secker17 <Secker17@users.noreply.github.com>

---
+++ public/index.js
@@ -0,0 +1,1 @@
+ReactDOM.render(<h1>Dette er en test</h1>, document.getElementById("root"));
--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import waterLogo from './logo.svg'; // Import your water-themed logo
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={waterLogo} className="App-logo" alt="water-logo" />
+        <h1>Welcome to the World of Water</h1>
+        <p>Explore the wonders of water and its importance in our lives.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.worldwatercouncil.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More about Water
         </a>
       </header>
     </div>
